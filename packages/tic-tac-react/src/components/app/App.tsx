744267550d853da3b4604c5ff5d3f4cac8f4cd2f
Piotr Gejgalis <piotr@gejgalis.com>
2020-10-24 09:04:53 +0200

    Added routing

--- packages/tic-tac-react/src/components/app/App.tsx
+++ packages/tic-tac-react/src/components/app/App.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 import 'common/css/styles.css';
 import { GameBoardUseState } from '../game-board-use-state/GameBoardUseState';
+import { GameBoardUseReducer } from '../game-board-use-reducer/GameBoardUseReducer';
 
 function App() {
   return (
     <div className="App">
-      <GameBoardUseState/>
+      <Router>
+        <Switch>
+          <Route path="/use-state">
+            <GameBoardUseState />
+          </Route>
+          <Route path="/use-reducer">
+            <GameBoardUseReducer />
+          </Route>
+          <Route path="/">
+            <ul>
+              <li><Link to="/use-state">useState</Link></li>
+              <li><Link to="/use-reducer">useReducer</Link></li>
+            </ul>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
